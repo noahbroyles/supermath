bdfdeac0bc4138cff63d785fee4ae9237a57afbf
Noah Broyles <noah@javamate.net>
2020-09-24 13:20:26 -0400

    Update supermath.py

--- supermath.py
+++ supermath.py
@@ -232,3 +232,28 @@ def stats(list):
 
     # Return all the values
     return mean, median, r, mode, standard, variance
+
+def isPrime(n):
+    # Miller-Rabin primality test implemented in Python
+    if n == 2:
+        return True
+
+    if n % 2 == 0:
+        return False
+
+    r, s = 0, n - 1
+    while s % 2 == 0:
+        r += 1
+        s //= 2
+    for _ in range(40): # 40 is a good amount of times to test for primality
+        a = random.randrange(2, n - 1)
+        x = pow(a, s, n)
+        if x == 1 or x == n - 1:
+            continue
+        for _ in range(r - 1):
+            x = pow(x, 2, n)
+            if x == n - 1:
+                break
+        else:
+            return False
+    return True
